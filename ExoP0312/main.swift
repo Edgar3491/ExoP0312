4f99dd3b05a814b14cc8c88994006bc48b03ac10
Edgar Cheikh <cheikh.kamel@gmail.com>
2018-11-30 14:56:46 +0100

    file created

---
+++ ExoP0312/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExoP0312
+//
+//  Created by Kamal CHEIKH on 30/11/2018.
+//  Copyright © 2018 Kamal CHEIKH. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
