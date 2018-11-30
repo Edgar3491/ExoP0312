aa248cfdfc8cbfeef36a010c7240abf4fbcb39b2
Edgar Cheikh <cheikh.kamel@gmail.com>
2018-11-30 15:00:37 +0100

    code for 100 bottles

--- ExoP0312/main.swift
+++ ExoP0312/main.swift
@@ -1,12 +1,16 @@
-//
-//  main.swift
-//  ExoP0312
-//
-//  Created by Kamal CHEIKH on 30/11/2018.
-//  Copyright © 2018 Kamal CHEIKH. All rights reserved.
-//
-
-import Foundation
-
-print("Hello, World!")
+   for i in 1...100 {
+    let n = 100 - i
+    let r = 99 - i
+    let v = i - 1
+    if n > 1 {
+        print("\(n) bottles of beer on the wall, \(n) bottles of beer.")
+        print("Take one down and pass it around, \(r) bottles of beer on the wall.")
+    } else if n == 1 {
+        print("\(n) bottle of beer on the wall, \(n) bottle of beer.")
+        print("Take one down and pass it around, no more bottles of beer on the wall.")
+    } else {
+        print("No more bottles of beer on the wall, no more bottles of beer.")
+        print("Go to the store and buy some more, \(v) bottles of beer on the wall.")
+    }
+   }
 
